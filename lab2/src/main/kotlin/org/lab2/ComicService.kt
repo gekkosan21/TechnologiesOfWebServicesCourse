886438440a3282344c091f631e6b98010bec78a0
Artem <it.art.den@gmail.com>
2025-03-24 00:12:34 +0300

    Add lab2

---
+++ lab2/src/main/kotlin/org/lab2/ComicService.kt
@@ -0,0 +1,62 @@
+package org.lab2
+
+import com.sun.xml.ws.fault.SOAPFaultBuilder
+import java.sql.SQLException
+import javax.jws.WebMethod
+import javax.jws.WebService
+
+
+@WebService(serviceName = "ComicService")
+class ComicService {
+    var soapFaultBuilder: SOAPFaultBuilder? = null
+
+    @WebMethod(operationName = "getAll")
+    open fun getAll(): List<ComicLib> {
+        val dao = ComicDAO()
+        return dao.listAllComics()
+    }
+
+    @WebMethod(operationName = "createComic")
+    open fun createComic(comic: ComicLib?): Boolean {
+        val comicDAO = ComicDAO()
+        try {
+            return comicDAO.insertComic(comic!!)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            return false
+        }
+    }
+
+    @WebMethod(operationName = "getComic")
+    open fun getComic(id: Int): ComicLib? {
+        val comicDAO = ComicDAO()
+        try {
+            return comicDAO.getComic(id)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    @WebMethod(operationName = "updateComic")
+    open fun updateComic(comic: ComicLib?): Boolean {
+        val comicDAO = ComicDAO()
+        try {
+            return comicDAO.updateComic(comic!!)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            return false
+        }
+    }
+
+    @WebMethod(operationName = "deleteComic")
+    open fun deleteComic(id: Int): Boolean {
+        val comicDAO = ComicDAO()
+        try {
+            return comicDAO.deleteComic(id)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            return false
+        }
+    }
+}
